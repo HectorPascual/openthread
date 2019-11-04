417e63ea5b0b95a7b907577a65669d6e68e2b398
Hector <rcpascualhector@gmail.com>
2019-11-04 11:16:31 +0100

    Now build_flash, flashes boards in parallel

--- build_flash.sh
+++ build_flash.sh
@@ -1,5 +1,9 @@
 make -f examples/Makefile-cc2538 JOINER=1 COMMISSIONER=1 COAP=1 
-arm-none-eabi-objcopy -O binary output/cc2538/bin/ot-cli-ftd output/cc2538/bin/ot-cli-ftd.bin 
-sudo python ../cc2538-bsl/cc2538-bsl.py -e -w -v -p /dev/ttyUSB$1 -a 0x00200000  output/cc2538/bin/ot-cli-ftd.bin
-sudo python -m serial.tools.miniterm /dev/ttyUSB$1 115200
+sudo arm-none-eabi-objcopy -O binary output/cc2538/bin/ot-cli-ftd output/cc2538/bin/ot-cli-ftd.bin 
+
+ls /dev/ttyUSB* | grep -oP "USB\K\d" | while read line; do
+ sudo python ../cc2538-bsl/cc2538-bsl.py -e -w -v -p /dev/ttyUSB$line -a 0x00200000  output/cc2538/bin/ot-cli-ftd.bin &
+done
+
+
 
